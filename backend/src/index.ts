916c4ca53b384c4bace88ea2e19b3edc2f6ce26b
Satyam Bansal <satyambansal008@gmail.com>
2026-09-02 19:44:53 +0530

    primsa config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,54 @@
 import express from "express";
 import dotenv from "dotenv";
-import { basename } from "node:path";
+import multer from "multer";
+import { PDFParse } from "pdf-parse";
 dotenv.config();
 
 const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 app.use(express.json());
 const PORT = process.env.PORT;
 
-
 //admin enpoints
- 
 
-app.post("/createchat")
+//creates knowledgebase
+app.post("/knowledgebase", (req, res) => {
+  //step1 : it will take the orgname, knowbname, address, type
+  //validation
+  //creates new knowledge base
+  //return knowledgebase
+});
+
+//fetches the docs
+app.post("/docs:orgId", upload.single("pdfFile"), async (req, res) => {
+
+  //multer will have the pdf in memory
+  if (!req.file) return res.status(400).send("No file uploaded.");
+  if (req.file.mimetype !== "application/pdf")
+    return res.status(400).send("Only PDF files allowed.");
+
+  //getting pdf from storage
+  const pdf = req.file.buffer;
+  
+  // const orgId = req.params;
+  // cosnt {category,type}
+
+  const parser = new PDFParse({
+    data: new Uint8Array(pdf),
+  });
+
+  const result = await parser.getText();
+  console.log(result);
+
+  //passed that text to embeeding model
+  //chunks and storage in vector db 
+  //return document embeed success
+});
+
 
 app.get("/", (req, res) => {
   res.json("I am healthy");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
