6ae06ac9c33982f6da9f787a26027da471967c72
Satyam Bansal <satyambansal008@gmail.com>
2026-09-09 00:38:43 +0530

    document upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { errorHandler } from "./middlewares/error.middleware";
+import orgRoutes from "./route/org.routes"
+import userRoutes from "./route/user.routes"
 dotenv.config();
 
 const app = express();
@@ -12,8 +14,11 @@ app.get("/", (req, res) => {
   res.json("I am healthy");
 });
 
+app.use("/org",orgRoutes);
+app.use("/user",userRoutes);
 app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
